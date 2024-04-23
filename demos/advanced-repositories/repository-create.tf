e1178bca30def530beaabe939de4730d5acd6441
Dakar Tal <dakart@jfrog.com>
2024-04-24 01:12:47 +0300

    remove token variable since using JFROG_ACCESS_TOKEN

--- demos/advanced-repositories/repository-create.tf
+++ demos/advanced-repositories/repository-create.tf
@@ -11,18 +11,14 @@ variable "artifactory_url" {
   type = string
 }
 
-variable "artifactory_access_token" {
-  type = string
-}
 
 # Configure the Artifactory provider
 provider "artifactory" {
   url           = var.artifactory_url
-  access_token  = var.artifactory_access_token
 }
 
 # Create a new repository
 resource "artifactory_local_maven_repository" "maven_repository" {
-  key             = "bleuteam-generic-release-local"
+  key             = "orange-maven-release-local"
   description     = "Maven repository created through Terraform"
 }
